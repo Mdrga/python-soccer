4940b8493cd68c62b16a634cd4d01613ef8c31a3
rainier-m <rainier.madruga@gmail.com>
2014-08-26 13:30:33 -0400

    Revert fe34423..8465a9d
    This rolls back to commit fe3442306336e7f6d6803f989f84987b0bfeb874.

--- parseBBC-PremierLeague.py
+++ parseBBC-PremierLeague.py
@@ -425,7 +425,7 @@ print localTeamFile
     downloadImage(homeBadgeURL, homeBadge)
 
 # print matchSoup.prettify()
-'''
+
 
 # Comment out to work on Team Details Parsing
 with open(outputNext, "w") as f:
@@ -442,3 +442,4 @@ for i in teamURLs:
 	with open(outputLast, "a") as f:
 		f.write(teamParse(i,'L') + '\n')
 		f.close()
+'''
