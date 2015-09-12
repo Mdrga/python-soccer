c232d71a185409b193b1c2d6deadde98c65ec223
Rainier M <rainier.madruga@gmail.com>
2015-09-12 12:14:10 -0400

    0.03.d - Pull Results and Parse
    Update the main script to parse team results.

--- parseBarclays2015.py
+++ parseBarclays2015.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 '''
 Created on Aug 16, 2015
-Modified on Aug 29, 2015
-Version 0.03.c
+Modified on Sep 12, 2015
+Version 0.03.d
 @author: rainier.madruga@gmail.com
 A simple Python Program to scrape the ESPN FC website for content.
 '''
 # Import Libraries needed for Scraping the various web pages
 import bs4 
+import re
 import datetime
 import time
 import requests
@@ -52,6 +53,7 @@ injuriesURL = 'http://www.fantasyfootballscout.co.uk/fantasy-football-injuries/'
 teamNewsURL = 'http://www.fantasyfootballscout.co.uk/team-news/'
 bbcURL = 'http://www.bbc.com/sport/0/football/premier-league/'
 bbcFixturesURL = 'http://www.bbc.com/sport/football/premier-league/fixtures'
+bbcResultsURL = 'http://www.bbc.com/sport/football/premier-league/results'
 
 # Base Path for Output
 localPath = 'D:\\ESPN-Parser\\'
@@ -60,6 +62,7 @@ workBook = openpyxl.load_workbook(os.path.join(localPath + baseWkBk))
 teamSheet = workBook.get_sheet_by_name('teams')
 playerSheet = workBook.get_sheet_by_name('players')
 matchSheet = workBook.get_sheet_by_name('matches')
+fixtureSheet = workBook.get_sheet_by_name('fixtures')
 
 # Create BS4 Object from ESPN Web Page
 espnRes = requests.get(espnURL)
@@ -88,42 +91,85 @@ for i in teamList:
 	teamURL = i.a['href']
 	teamSheet.cell('A' + str(teamCounter)).value = teamName
 	teamSheet.cell('B' + str(teamCounter)).value = teamURL
-	print (teamName)
+	# print (teamName)
 	teamCounter += 1
 
 workBook.save(os.path.join(localPath + ds + '.xlsx'))
 print (hr)
 
-# Parse BBC Results and Fixtures
+# Parse BBC Fixtures and Results
 bbcFixtures = requests.get(bbcFixturesURL)
 bbcFixtures.raise_for_status()
 
+bbcResults = requests.get(bbcResultsURL)
+bbcResults.raise_for_status()
+
 bbcFixtureSoup = bs4.BeautifulSoup(bbcFixtures.text, "html.parser")
-with open(os.path.join(localPath + 'bbcLanding.html'), 'wb') as fo:
+with open(os.path.join(localPath + 'bbcFixtures.html'), 'wb') as fo:
 	for chunk in bbcFixtures.iter_content(100000):
 		fo.write(chunk)
-		print ('Writing file...')
+		print ('Writing fixtures file...')
+
+print (shr)
+
+bbcResultsSoup = bs4.BeautifulSoup(bbcResults.text, "html.parser")
+with open (os.path.join(localPath + 'bbcResults.html'), 'wb') as fo:
+	for chunk in bbcResults.iter_content(100000):
+		fo.write(chunk)
+		print ('Writing results file...')
 
 print (hr)
 
 fixtureContainer = bbcFixtureSoup.find('div', class_="fixtures-table full-table-medium")
 fixtureRow = fixtureContainer.find_all('table', class_="table-stats")
 
+fixtureCount = 2
+
 for i in fixtureContainer:
 	rowData = i
 	if len(rowData) == 1:
 		fixtureDate = str((rowData.string).strip())
 		if len(fixtureDate) > 1:
-			print (fixtureDate + '| ' + str(len(fixtureDate)))
-
-
+			dateYear = fixtureDate[-4:]
+			dayOfWeek = fixtureDate[0:3]
+			dayOfMonth = re.findall(r'\d{1,2}', fixtureDate)
+			dayOfMonth = dayOfMonth[0]
+			month = re.findall(r'[A-Z][a-z]*', fixtureDate)
+			month = month[1]
+			printFixtureDate = str(dateYear) + ' ' + month[0:3] + ' ' + str(dayOfMonth) + ' ' + dayOfWeek
+			print (printFixtureDate)
+			print (shr)
 	if len(rowData) > 1:
 		# print (rowData)
 		tableRow = rowData.find_all('tr')
 		previewData = i.find_all('tr', class_="preview")
 		for i in previewData:
-			# print (i)
-			print ('***========***')
-	print (shr)
+			# Identify the information about the Home Team
+			homeTeam = i.find('span', class_="team-home teams")
+			homeTeamURL = homeTeam.a['href']
+			homeTeamName = homeTeam.a.get_text()
+			
+			# Identify the information about the Away Team
+			awayTeam = i.find('span', class_="team-away teams")
+			awayTeamURL = awayTeam.a['href']
+			awayTeamName = awayTeam.a.get_text()
+			
+			# Identify Kickoff Time and Status
+			kickoff = i.find('td', class_="kickoff")
+			kickoff = kickoff.get_text().strip()
+			status = i.find('td', class_="status")
+			status = status.get_text().strip()
+			# print (len(status))
+			# Output Home Team, Away Team, Match Date, Kickoff Time, Match Status
+			fixtureSheet.cell('A' + str(fixtureCount)).value = homeTeamName
+			fixtureSheet.cell('B' + str(fixtureCount)).value = awayTeamName
+			fixtureSheet.cell('C' + str(fixtureCount)).value = printFixtureDate
+			fixtureSheet.cell('D' + str(fixtureCount)).value = kickoff
+			fixtureSheet.cell('E' + str(fixtureCount)).value = status
 
+			# print (homeTeamName + ' v. ' + awayTeamName + ' ' + status + ' :: ' + kickoff)
+			# print (shr)
+			fixtureCount += 1
+	
+workBook.save(os.path.join(localPath + ds + '.xlsx'))
 print (hr)
